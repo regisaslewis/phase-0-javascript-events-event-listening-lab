ac8458615e53af0cae4888e4d2b7d6c4e747c260
RegisLewis <regisaslewis@gmail.com>
2023-01-09 20:41:56 -0800

    commit tree 54ea907f46a225d6485af2d3811c24abae451a90

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
-}
+    const input = document.getElementById("button");
+    function alertOnClick() {
+        alert("I was clicked!");
+    };
+    input.addEventListener("click", alertOnClick);  
+};
